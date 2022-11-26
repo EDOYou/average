8c2320d999718d529661f42999abf486e471e003
Taghiyev <ktaghiyev01@gmail.com>
2022-11-26 19:48:53 +0100

    average

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -6,7 +6,18 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        Scanner sc = new Scanner(System.in);
+        int sum = 0; // Sum of all elements except for 0
+        int cnt = 0; // Counts the number of elements except for 0
+        int average;
 
+        while (true) {
+            int num = sc.nextInt();
+            if (num == 0) break;
+            sum += num;
+            cnt++;
+        }
+        average = sum / cnt;
+        System.out.println(average);
     }
-
 }
